1c9822a28ee3f7a143c2c2adf863ef119d1a729d
Maruf Ahmed Sujib <maruf.ahmed@nexdecade.com>
2023-09-24 11:08:22 +0600

    test purpose

---
+++ src/main/java/nex/vts/backend/models/vehicle/Vehicle_Location.java
@@ -0,0 +1,15 @@
+package nex.vts.backend.models.vehicle;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+public class Vehicle_Location {
+
+
+}
