0ee1ef05923020e209ae2e7da1675144d0f40a96
MBrinto <mahzabin.ferdous@nexdecade.com>
2023-10-12 12:19:25 +0600

    Custom Error Code Details is Updated

--- src/main/java/nex/vts/backend/controllers/DriverInfoController.java
+++ src/main/java/nex/vts/backend/controllers/DriverInfoController.java
@@ -4,9 +4,7 @@ package nex.vts.backend.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nex.vts.backend.models.responses.BaseResponse;
-import nex.vts.backend.models.responses.DriverInfoModel;
-import nex.vts.backend.models.responses.GetExpansesListObj;
-import nex.vts.backend.models.responses.GetExpansesModel;
+
 import nex.vts.backend.repositories.GetExpenseHeaderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -29,20 +27,20 @@ public class DriverInfoController {
     public ResponseEntity<String> vehicleList(@PathVariable("id") Integer id) throws JsonProcessingException {
 
 
-       // Optional<ArrayList<DriverInfoModel>> GetDriverInfo = repo.findAllExpenses(id);
+        Optional<ArrayList<DriverInfoModel>> GetDriverInfo = repo.findAllExpenses(id);
         BaseResponse baseResponse = new BaseResponse();
 
-/*
+
         if (GetDriverInfo.isEmpty()) {
             baseResponse.status = false;
-            baseResponse.errorMsg = "Data  not found";
+            baseResponse.errorMsg = " User Data  not found";
             baseResponse.errorCode = 4041;
         } else {
             GetDriverInfoObj getExpansesListObj = new GetDriverInfoObj();
             baseResponse.status = true;
             GetDriverInfoObj.setGetDriverModels(GetDriverInfo);
             baseResponse.data =  GetDriverInfoObj;
-        }*/
+        }
 
 
         return ResponseEntity.ok().body(objectMapper.writeValueAsString(baseResponse));
