200d69f8d25e52f85fe5263aeb488d0a0bf65000
A T M Faruque Mithu <mithu@nexdecade.com>
2024-01-21 18:20:28 +0600

    Starting message has been added

--- src/main/java/nex/vts/backend/VtsBackendApplication.java
+++ src/main/java/nex/vts/backend/VtsBackendApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+import java.util.Date;
+
 @SpringBootApplication
 @EnableRetry
 public class VtsBackendApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(VtsBackendApplication.class, args);
+
+        System.out.println("\n============================================");
+        System.out.println("Welcome to the VTS APIs Management System");
+        System.out.println("Date in GMT: " + new Date());
+        System.out.println("============================================\n");
+
+        System.out.println("APIs Service start...\n");
+
     }
 }
