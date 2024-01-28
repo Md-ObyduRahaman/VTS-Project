c0d798f9178f3434c227527716e06447420351c6
Maruf Ahmed Sujib <maruf.ahmed@nexdecade.com>
2024-01-28 17:23:57 +0600

    fixed naming convention

---
+++ src/main/java/nex/vts/backend/models/responses/TrackNowResponseTwo.java
@@ -0,0 +1,35 @@
+package nex.vts.backend.models.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class TrackNowResponseTwo {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("lat")
+    private Double lat;
+
+    @JsonProperty("lon")
+    private Double longs;
+
+    @JsonProperty("speed")
+    private Double speed;
+
+    @JsonProperty("head")
+    private String head;
+
+    @JsonProperty("timeStamp")
+    private String timeStamp;
+
+    @JsonProperty("datetime")
+    private String datetime;
+
+    @JsonProperty("engine")
+    private String engine;
+
+    @JsonProperty("locationPoi")
+    private String locDetails;
+}
