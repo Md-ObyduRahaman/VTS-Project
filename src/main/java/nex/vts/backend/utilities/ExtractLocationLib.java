fdf148535f859fc94791d7e459dfe9754b40021f
A T M Faruque Mithu <mithu@nexdecade.com>
2024-02-01 13:11:27 +0600

    POI Location Class => ExtractLocationLib

--- src/main/java/nex/vts/backend/utilities/ExtractLocationLib.java
+++ src/main/java/nex/vts/backend/utilities/ExtractLocationLib.java
@@ -4,8 +4,17 @@ public class ExtractLocationLib {
 
     public static String get_Location(String _lat, String _lon) {
 
+        String res = get_Location_HTTP(_lat, _lon);
+
+        return res; //_lat + "," + _lon;
+    }
+
+
+    public static String get_Location_HTTP(String _lat, String _lon) {
+
         return _lat + "," + _lon;
     }
+
 
 
     private static String _XY() {
