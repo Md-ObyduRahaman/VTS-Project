6981d24c5e8578d0c5f594844542337024a3065c
Maruf Ahmed Sujib <maruf.ahmed@nexdecade.com>
2024-02-01 10:59:00 +0600

    fixed speed data

--- src/main/java/nex/vts/backend/services/TrackHistoryService.java
+++ src/main/java/nex/vts/backend/services/TrackHistoryService.java
@@ -58,6 +58,7 @@ public class TrackHistoryService {
                             String.valueOf(currentLocation.get().getLat()),
                             String.valueOf(currentLocation.get().getLongs())
                     ));
+                    responseTwo.setSpeed(currentLocation.get().getSpeed());
 
                     trackNowResponse.setVehicleCurrentLocation(Optional.ofNullable(responseTwo));
 
