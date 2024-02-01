541128003eb6c697b2651a9333b74dcc11f243a2
Md. Obydur Rahman Sojib <obydur.rahman@nexdecade.com>
2024-02-01 13:30:16 +0600

    lat lon api

--- src/main/java/nex/vts/backend/utilities/ExtractLocationLib.java
+++ src/main/java/nex/vts/backend/utilities/ExtractLocationLib.java
@@ -1,5 +1,14 @@
 package nex.vts.backend.utilities;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 public class ExtractLocationLib {
 
     public static String get_Location(String _lat, String _lon) {
@@ -10,9 +19,19 @@ public class ExtractLocationLib {
     }
 
 
-    public static String get_Location_HTTP(String _lat, String _lon) {
+    public static String get_Location_HTTP(String _lat, String _lon) throws IOException {
 
-        return _lat + "," + _lon;
+
+        // Starting fetch data
+        HttpClient httpClient = HttpClients.createDefault();
+        String apiUrl = "https://vts2.m2mbd.com/m_backend/rev_geocoder.php?lat="+_lat+"&lon="+_lon;
+        HttpGet httpGet = new HttpGet(apiUrl);
+        //  httpGet.setHeader(HttpHeaders.AUTHORIZATION, jwtToken);
+        HttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        String area = EntityUtils.toString(entity);
+
+        return area;
     }
 
 
